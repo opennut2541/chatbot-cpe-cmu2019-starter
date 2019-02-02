a7a6b8938a0845b55621a95ebd37478f4046768b
Charin Sansri <Nut_charlin@hotmail.co.th>
2019-02-02 15:16:53 +0700

    asd
    asdasda

--- server.js
+++ server.js
@@ -22,42 +22,54 @@ app.post('/webhook', middleware(config), (req, res) => {
     if (event.type === 'message') {
         const message = event.message;
         console.log(message);
-        client.replyMessage(event.replyToken, 
-          {
-            "type": "template",
-            "altText": "This is a buttons template",
-            "template": {
-                "type": "buttons",
-                "thumbnailImageUrl": "https://i5.walmartimages.ca/images/Large/580/6_r/875806_R.jpg",
-                "imageAspectRatio": "rectangle",
-                "imageSize": "cover",
-                "imageBackgroundColor": "#5C42F4",
-                "title": "Charin Sansri",
-                "text": "เลือกซักอย่าง",
-                "defaultAction": {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "https://i5.walmartimages.ca/images/Large/580/6_r/875806_R.jpg"
-                },
-                "actions": [
-                    {
-                      "type": "message",
-                      "label": "มุขนี้ซื้อ",
-                      "text": "ใจเย็นไอ้สอง"
-                    },
-                    {
-                      "type": "uri",
-                      "label": "Facebook",
-                      "uri": "https://www.facebook.com/NuTCharinSunsri"
-                    },
-                    {
-                      "type": "uri",
-                      "label": "CPE",
-                      "uri": "http://cpe.eng.cmu.ac.th/2013/"
-                    }
-                ]
-            }
-          })
+        client.replyMessage(event.replyToken, {
+          "type": "template",
+          "altText": "this is a carousel template",
+          "template": {
+              "type": "carousel",
+              "columns": [
+                  {
+                    "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+                    "imageBackgroundColor": "#FFFFFF",
+                    "title": "this is menu",
+                    "text": "description",
+                    "actions": [
+                        {  
+                            "type":"cameraRoll",
+                            "label":"Camera roll"
+                        },
+                        {  
+                          "type":"location",
+                          "label":"Location"
+                       }
+                    ]
+                  },
+                  {
+                    "thumbnailImageUrl": "https://c.76.my/Malaysia/line-brown-bear-cute-pencil-case-ubiyo-1802-02-Ubiyo@6.jpg",
+                    "imageBackgroundColor": "#000000",
+                    "title": "this is menu",
+                    "text": "description",
+                    "actions": [
+                      {
+                        "type":"datetimepicker",
+                        "label":"Select date",
+                        "data":"storeId=12345",
+                        "mode":"datetime",
+                        "initial":"2017-12-25t00:00",
+                        "max":"2018-01-24t23:59",
+                        "min":"2017-12-25t00:00"
+                      },
+                      {  
+                        "type":"camera",
+                        "label":"Camera"
+                     }
+                  ]
+                  }
+              ],
+              "imageAspectRatio": "rectangle",
+              "imageSize": "cover"
+          }
+      })
     }
     
 
