36e2d24e6ee2de4dde2fef3e568e5a5347c7c897
Charin Sansri <Nut_charlin@hotmail.co.th>
2019-02-02 14:38:39 +0700

    saldhaks
    asdfmmasnkdf

--- server.js
+++ server.js
@@ -22,11 +22,42 @@ app.post('/webhook', middleware(config), (req, res) => {
     if (event.type === 'message') {
         const message = event.message;
         console.log(message);
-        client.replyMessage(event.replyToken, {
-          type: 'sticker',
-          packageId: "1",
-          stickerId: "1"
-      })
+        client.replyMessage(event.replyToken, 
+          {
+            "type": "template",
+            "altText": "This is a buttons template",
+            "template": {
+                "type": "buttons",
+                "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover",
+                "imageBackgroundColor": "#FFFFFF",
+                "title": "Menu",
+                "text": "Please select",
+                "defaultAction": {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://google.com/"
+                },
+                "actions": [
+                    {
+                      "type": "postback",
+                      "label": "Buy",
+                      "data": "action=buy&itemid=123"
+                    },
+                    {
+                      "type": "message",
+                      "label": "Add to cart",
+                      "text": "no no no"
+                    },
+                    {
+                      "type": "uri",
+                      "label": "View detail",
+                      "uri": "http://google.com"
+                    }
+                ]
+            }
+          })
     }
     
 
