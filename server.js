73d5d4264105faa7eac632a68c85efa4f009b395
Charin Sansri <Nut_charlin@hotmail.co.th>
2019-02-02 15:57:49 +0700

    jj
    jj

--- server.js
+++ server.js
@@ -2,80 +2,76 @@ const express = require('express')
 const middleware = require('@line/bot-sdk').middleware
 const app = express()
 const Client = require('@line/bot-sdk').Client;
+const restClient = new (require('node-rest-client').Client)
 
+require('dotenv').config()
 
 const config = {
   channelAccessToken: 'n88hInBJRyYcNr6+hH9xfd8CVmjS1aChz/ddo2cZRTMFDBk+BYDGJG1Cys9vX6Z54V35K8lFPsXDofI8Jkh/YbfC5DxW2NlIMMRTTMUB31isF7VpFNsNzz8a57iXCC1Ejnc6qEPlZR+RP6BkFM1ETgdB04t89/1O/w1cDnyilFU=',
   channelSecret: '1e12d29941fc79f3bfdd367a18045df9'
   
 }
-const client  = new Client(config)
+const client = new line.Client(config);
+
 app.get('/', function (req, res) {
-    res.send('Hello World!!')
-    })
+	res.send('03-pm2.5-bot')
+})
 
-app.post('/webhook', middleware(config), (req, res) => {
-  //req.body.events // webhook event objects
-  //req.body.destination // user ID of the bot (optional)
-  res.send("Webhook success")
-  const event = req.body.events[0];
-    if (event.type === 'message') {
-        const message = event.message;
-        console.log(message);
-        client.replyMessage(event.replyToken, {
-          "type": "template",
-          "altText": "this is a carousel template",
-          "template": {
-              "type": "carousel",
-              "columns": [
-                  {
-                    "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
-                    "imageBackgroundColor": "#FFFFFF",
-                    "title": "this is menu",
-                    "text": "description",
-                    "actions": [
-                        {  
-                            "type":"cameraRoll",
-                            "label":"Camera roll"
-                        },
-                        {  
-                          "type":"location",
-                          "label":"Location"
-                       }
-                    ]
-                  },
-                  {
-                    "thumbnailImageUrl": "https://c.76.my/Malaysia/line-brown-bear-cute-pencil-case-ubiyo-1802-02-Ubiyo@6.jpg",
-                    "imageBackgroundColor": "#000000",
-                    "title": "this is menu",
-                    "text": "description",
-                    "actions": [
-                      {
-                        "type":"datetimepicker",
-                        "label":"Select date",
-                        "data":"storeId=12345",
-                        "mode":"datetime",
-                        "initial":"2017-12-25t00:00",
-                        "max":"2018-01-24t23:59",
-                        "min":"2017-12-25t00:00"
-                      },
-                      {  
-                        "type":"camera",
-                        "label":"Camera"
-                     }
-                  ]
-                  }
-              ],
-              "imageAspectRatio": "rectangle",
-              "imageSize": "cover"
-          }
-      })
-    }
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch(err => console.log('err', err))
+});
+
+function handleEvent(event) {
+  if(event.type === 'message' && event.message.type === 'location') {
+    return handleLocationEvent(event)
+  }else {
+    return Promise.resolve(null)
+  }
+}
+
+function handleLocationEvent(event) {
+  return new Promise((resolve, reject) => {
+    restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
+      if (data) {
+        const pinData = data.map(row => ({
+          "thumbnailImageUrl": row.aqi.icon,
+          "imageBackgroundColor": "#FFFFFF",
+          "title": `PM 2.5: ${row.aqi.aqi}`,
+          "text": `${row.nameTH}, ${row.areaTH}`,
+          "actions": [
+            {
+              "type": "uri",
+              "label": "ข้อมูลย้อนหลัง",
+              "uri": row.historyUrl
+            }
+          ]
+        }))
     
+        var msg = {
+          "type": "template",
+          "altText": "ข้อมูลสถานที่",
+          "template": {
+            "type": "carousel",
+            "columns": pinData,
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover"
+          }
+        }
 
+        resolve(client.replyMessage(event.replyToken, msg))
+      } else {
+        reject()
+      }
+    })
   })
+ 
+}
 
-  app.set('port', (process.env.PORT || 4000))
+app.set('port', (process.env.PORT || 4000))
+
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
 })
