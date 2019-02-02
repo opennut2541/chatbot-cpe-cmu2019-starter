1c45409ac0b913ff7f2a2b088ce8b90413893751
Charin Sansri <Nut_charlin@hotmail.co.th>
2019-02-02 13:38:17 +0700

    nnb
    mnjk

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const middleware = require('@line/bot-sdk').middleware
 const app = express()
-const Client = require('@line/bot-sdk').Client;
+const client = require('@line/bot-sdk').Client;
 
 const config = {
   channelAccessToken: 'n88hInBJRyYcNr6+hH9xfd8CVmjS1aChz/ddo2cZRTMFDBk+BYDGJG1Cys9vX6Z54V35K8lFPsXDofI8Jkh/YbfC5DxW2NlIMMRTTMUB31isF7VpFNsNzz8a57iXCC1Ejnc6qEPlZR+RP6BkFM1ETgdB04t89/1O/w1cDnyilFU=',
